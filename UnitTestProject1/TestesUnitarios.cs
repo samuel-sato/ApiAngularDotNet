a4cb2ebf578981168f03427298601e63522acd72
Samuel Sato <samuelsato2009@gmail.com>
2023-12-04 21:42:29 -0300

    Teste Unitário Cadastrar Carro

---
+++ UnitTestProject1/TestesUnitarios.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestesUnitarios
+    {
+        [TestMethod]
+        public void CasdastraCarro()
+        {
+            try
+            {
+                Car c1 = new Car(new Guid(), 2018, "Ford Ka", "ABC2D19");
+                CarService service = CarService();
+                service.Add(c1);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
